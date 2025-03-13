bcd75c220385509fa1794650fda42928412fcafb
jhchoi <jhchoi@deali.net>
2025-03-13 12:08:20 +0900

    feat: config filePath를 Flag를 사용해 가져오자.

--- init/main.go
+++ init/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"flag"
 	"web-study/init/cmd"
 )
 
+var configFileFlag = flag.String("config", "config.toml", "config file not found")
+
 func main() {
-	cmd.NewCmd("./config.toml")
+	flag.Parse()
+	cmd.NewCmd(*configFileFlag)
 }

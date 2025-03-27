41d64e88a369b35edc853ff0e5857e48ddd504b5
jhchoi <jhchoi@deali.net>
2025-03-27 11:29:50 +0900

    feat: gin framework를 통한 Server Start

--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -1,23 +1,25 @@
 package cmd
 
 import (
-	"fmt"
 	"web-study/config"
+	"web-study/network"
 )
 
 type Cmd struct {
 	// 다른 network, repository, service, types, config 를 구성 할 예정
 	// cmd는 main에서 호출된다.
 
-	config *config.Config // config 를 선언
+	config  *config.Config   // config 를 선언
+	network *network.Network // network 선언
 }
 
 func NewCmd(filePath string) *Cmd {
 	c := &Cmd{
-		config: config.NewConfig(filePath),
+		config:  config.NewConfig(filePath),
+		network: network.NewNetwork(),
 	}
 
-	fmt.Println(c.config.Server.Port)
+	c.network.ServerStart(c.config.Server.Port)
 
 	return c
 }
--- network/root.go
+++ network/root.go
@@ -1,4 +1,21 @@
 package network
 
+import "github.com/gin-gonic/gin"
+
 // api를 만든다고 했을 때, 라우터 설정을 하는 곳
 // 다양한 API에 대한 root를 관리한다.
+
+type Network struct {
+	engin *gin.Engine
+}
+
+func NewNetwork() *Network {
+	r := &Network{
+		engin: gin.New(),
+	}
+	return r
+}
+
+func (r *Network) ServerStart(port string) error {
+	return r.engin.Run(port)
+}

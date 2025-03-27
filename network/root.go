ec6d3e602600ccd142b1f99663bc5198e2499a68
jhchoi <jhchoi@deali.net>
2025-03-27 12:02:04 +0900

    feat: network 에 user router 추가

--- network/root.go
+++ network/root.go
@@ -13,9 +13,29 @@ func NewNetwork() *Network {
 	r := &Network{
 		engin: gin.New(),
 	}
+
+	// network 에 user router 추가
+	newUserRouter(r)
+
 	return r
 }
 
 func (r *Network) ServerStart(port string) error {
 	return r.engin.Run(port)
+}
+
+func (n *Network) requestGET(path string, handler ...gin.HandlerFunc) gin.IRoutes {
+	return n.engin.GET(path, handler...)
+}
+
+func (n *Network) requestPOST(path string, handler ...gin.HandlerFunc) gin.IRoutes {
+	return n.engin.POST(path, handler...)
+}
+
+func (n *Network) requestPUT(path string, handler ...gin.HandlerFunc) gin.IRoutes {
+	return n.engin.PUT(path, handler...)
+}
+
+func (n *Network) requestDELETE(path string, handler ...gin.HandlerFunc) gin.IRoutes {
+	return n.engin.DELETE(path, handler...)
 }
---
+++ network/user.go
@@ -0,0 +1,61 @@
+package network
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"sync"
+)
+
+var (
+	// 라우터는 한번만 실행되야 하므로 sync.Once 설정 추가
+	// network Root에서 2번 호출되어도 1번만 실행된다.
+	userRouterInit     sync.Once
+	userRouterInstance *userRouter
+)
+
+type userRouter struct {
+	router *Network
+	// service
+}
+
+func newUserRouter(router *Network) *userRouter {
+	userRouterInit.Do(func() {
+		userRouterInstance = &userRouter{
+			router: router,
+		}
+
+		// Go 기본 모듈의 http.handleFunc("/", ) 과 동일한 액션
+		/* 아래처럼 선언해도 되지만, network 에 함수로 분리해 호출을 간소화 할 수 있다.
+		userRouterInstance.router.engin.POST("/", userRouterInstance.create)
+		userRouterInstance.router.engin.GET("/", userRouterInstance.get)
+		userRouterInstance.router.engin.PUT("/", userRouterInstance.update)
+		userRouterInstance.router.engin.DELETE("/", userRouterInstance.delete)*/
+
+		router.requestPOST("/", userRouterInstance.create)
+		router.requestGET("/", userRouterInstance.get)
+		router.requestPUT("/", userRouterInstance.update)
+		router.requestDELETE("/", userRouterInstance.delete)
+	})
+
+	return userRouterInstance
+}
+
+/*
+TIP: Go 에서의 접근제어자는 함수명을 보고 판단한다.
+(함수명의 시작이 대/소문자 인지를 보고 판단)
+*/
+func (u *userRouter) create(c *gin.Context) {
+	fmt.Println("create requested !")
+}
+
+func (u *userRouter) get(c *gin.Context) {
+	fmt.Println("get requested !")
+}
+
+func (u *userRouter) update(c *gin.Context) {
+	fmt.Println("update requested !")
+}
+
+func (u *userRouter) delete(c *gin.Context) {
+	fmt.Println("delete requested !")
+}
